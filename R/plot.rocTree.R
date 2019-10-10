f0fafbd2901af3ba5a1ebbe7d2e6ed7d4c6225ea
Sy Han Chiou <schiou@utdallas.edu>
2019-10-10 22:20:02 +0000

    version 1.1.0

--- R/plot.rocTree.R
+++ R/plot.rocTree.R
@@ -1,6 +1,8 @@
+globalVariables(c("nd", "hazard", "Survival")) ## global variables for plot.rocTree
+
 #' Plotting an \code{rocTree} object
 #'
-#' Plots an \code{rocTree} object. The function returns a \code{dgr_graph} object and is rendered in the RStudio Viewer.
+#' Plots an \code{rocTree} object. The function returns a \code{dgr_graph} object that is rendered in the RStudio Viewer or survival/hazard estimate at terminal nodes.
 #'   
 #' @param x an object of class "\code{rocTree}", usually returned by the rocTree function.
 #' @param output a string specifying the output type; graph (the default) renders the graph using the \code{grViz} function, and \code{visNetwork} renders the graph using the visnetwork function.
@@ -13,97 +15,122 @@
 #' @param savePlot is a logical value indicating whether the plot will be saved (exported); the default value is "FALSE".
 #' @param file_name is a character string specifying the name of the plot when "savePlot = TRUE". The file name should include its extension. The default value is "pic.pdf"
 #' @param file_type is a character string specifying the type of file to be exported. Options for graph files are: "png", "pdf", "svg", and "ps". The default value is "pdf".
+#' @param type is an optional character string specifying the type of plots to produce. The available options are "tree" for plotting survival tree (default),
+#' "survival" for plotting the estimated survival probabilities for the terminal nodes, and "hazard" for plotting the estimated hazard for the terminal nodes.
+#' The \code{dgr_graph} options are available only when \code{type = tree}.
+#' @param tree is an optional integer specifying the \eqn{n^{th}} tree in the forest to print.
 #' @param ... arguments to be passed to or from other methods.
 #'
 #' @seealso See \code{\link{rocTree}} for creating \code{rocTree} objects.
+#' @importFrom DiagrammeR export_graph render_graph
+#' @importFrom data.tree SetGraphStyle SetNodeStyle
+#' @importFrom ggplot2 labs geom_smooth
+#' 
 #' @export
-#' @examples
-#' set.seed(1)
-#' dat <- simu(100, 0, 1.3)
-#' library(survival)
-#' system.time(fit <- rocTree(Surv(Time, death) ~ z1 + z2, id = id,
-#' data = dat, control = list(prune = TRUE, nflds = 10)))
-#' plot(fit)
-#' plot(fit, rankdir = "LR", nodeOnly = TRUE)
-#' plot(fit, output = "visNetwork", nodeOnly = TRUE, shape = "box")
+#' @example inst/examples/ex_plot_rocTree.R
 plot.rocTree <- function(x, output = c("graph", "visNetwork"),
-                         digits = 4,
+                         digits = 4, tree = 1L,
                          rankdir = c("TB", "BT", "LR", "RL"),
                          shape = "ellipse",
                          nodeOnly = FALSE,
                          savePlot = FALSE, 
                          file_name = "pic.pdf",
-                         file_type = "pdf", ...) {
+                         file_type = "pdf",
+                         type = c("tree", "survival", "hazard"),...) {
     if (!is.rocTree(x)) stop("Response must be a \"rocTree\" object")
     output <- match.arg(output)
+    type <- match.arg(type)
     rankdir <- match.arg(rankdir)
-    Frame <- x$Frame
-    ## create data.tree
-    root <- Node$new("Root", type = "root", decision = "", nd = 1)
-    for (i in 2:nrow(Frame)) {
-        if (i <= 3) parent <- "root"
-        if (i > 3) parent <- paste0("Node", Frame$nd[i] %/% 2)
-        if (Frame$terminal[i] == 2) {
-            type <- "terminal"
-            display <- with(Frame, paste0(nd[i], ") ", tree.split.names(nd[i], nd, p, cut, x$vNames, digits), "*"))
-        } else {
-            type <- "interior"
-            display <- with(Frame, paste0(nd[i], ") ", tree.split.names(nd[i], nd, p, cut, x$vNames, digits)))
-        }
-        eval(parse(text = paste0("Node", Frame$nd[i], "<-", parent,
-                                 "$AddChild(display, type = type, nd = Frame$nd[i])")))
-    }
-    SetGraphStyle(root, rankdir = rankdir)
-    if (nodeOnly) {
-        GetNodeLabel <- function(node) {
-            switch(node$type,
-                   root = "Root", 
-                   terminal = paste0("Node ", node$nd),
-                   interior = paste0("Node ", node$nd))
-        }
-        SetNodeStyle(root, fontname = 'helvetica', label = GetNodeLabel, shape = shape)
+    if (x$ensemble) {
+        if (!is.wholenumber(tree)) stop("Tree number must be an integer.")
+        if (tree > length(x$trees)) stop("Tree number exceeded the number of trees in forest.")    
+        Frame <- x$Frame[[tree]]
     } else {
-        SetNodeStyle(root, fontname = 'helvetica', shape = shape)
+        Frame <- x$Frame
+        if (type == "survival") {
+            tmp <- data.frame(x$data$.Y0, x$data$.X0)[x$data$.D0 > 0,]
+            colnames(tmp) <- c(x$rName, x$vNames)
+            t0 <- tmp[,1]
+            atTerm <- lapply(split(tmp, x$nodeLabel), function(xx) {
+                xx <- xx[findInt(t0, xx[,1]),]
+                xx[,1] <- t0
+                rownames(xx) <- NULL
+                return(xx)
+            })
+            atTerm <- lapply(atTerm, function(xx)
+                data.frame(Time = t0, Survival = predict(x, newdata = xx)$survFun(t0)))
+            atTerm <- do.call(rbind, atTerm)
+            atTerm$nd <- as.factor(rep(sort(unique(x$nodeLabel)) + 1, each = length(t0)))
+            ## atTerm$nd <- as.factor(rep(sort(unique(x$nodeLabel)) + 1, table(x$nodeLabel)))
+            rownames(atTerm) <- NULL
+            gg <- ggplot(atTerm, aes(x = Time, y = Survival, col = nd)) + geom_step(lwd = I(1.1)) +
+                xlab("Time") + ylab("Survival probabilities") + labs(col = "Node")
+            return(gg)
+        }
+        if (type == "hazard") {
+            tmp <- data.frame(x$data$.Y0, x$data$.X0)[x$data$.D0 > 0,]
+            colnames(tmp) <- c(x$rName, x$vNames)
+            t0 <- tmp[,1]
+            atTerm <- lapply(split(tmp, x$nodeLabel), function(xx) {
+                xx <- xx[findInt(t0, xx[,1]),]
+                xx[,1] <- t0
+                rownames(xx) <- NULL
+                return(xx)
+            })
+            atTerm <- lapply(atTerm, function(xx)
+                data.frame(Time = t0, hazard = predict(x, newdata = xx, type = "haz")$hazFun(t0)))
+            atTerm <- do.call(rbind, atTerm)
+            atTerm$nd <- as.factor(rep(sort(unique(x$nodeLabel)) + 1, each = length(t0)))
+            rownames(atTerm) <- NULL
+            gg <- ggplot(atTerm, aes(x = Time, y = hazard, col = nd)) +
+                geom_smooth(method = "loess", se = FALSE) + 
+                xlab("Time") + ylab("Hazard estimates") + labs(col = "Nodes")
+            return(gg)
+        }
     }
-    ## x$graph <- ToDiagrammeRGraph(x, direction = "climb", pruneFun = NULL)
-    plot.Node(root, output = output, control = list(savePlot = savePlot, file_name = file_name, file_type = file_type))
-}
-
-#' Plotting the estimated hazard function from an rocTree object
-#'
-#' Plot the estimated hazard function from rocTree objects.
-#'
-#' @param x an object of class "rocTree", usually returned by the rocTree function.
-#' @param ghN an optional smoothing parameter used in smoothing hazard functions;
-#' the default value is 0.2.
-#' @export
-#'
-#' @importFrom ggplot2 ggplot geom_line xlab ylab aes
-#' 
-#' @examples
-#' set.seed(1)
-#' dat <- simu(100, 0, 1.3)
-#' library(survival)
-#' system.time(fit <- rocTree(Surv(Time, death) ~ z1 + z2, id = id,
-#' data = dat, control = list(prune = TRUE, nflds = 10)))
-#' plotTreeHaz(fit)
-plotTreeHaz <- function(x, ghN = NULL) {
-    if (!is.rocTree(x)) stop("Response must be a \"rocTree\" object")
-    if (!is.null(ghN)) x$parm@ghN <- ghN
-    x <- c(x, rocTree.haz(x$dfFinal, x$Y0, x$parm))
-    tmp <- data.frame(x = x$tt, y = unlist(x$r2Final),
-                      Node = rep(x$ndFinal, each = length(x$tt)), row.names = NULL)
-    tmp$Node <- factor(tmp$Node)
-    gg <- ggplot(tmp, aes(x = x, y = y, group = Node)) +
-        geom_line(aes(linetype = Node, color = Node), lwd = I(1.1)) +
-        xlab("Time") + ylab("Hazard")
-    gg
+    if (type == "tree") {    
+        ## create data.tree
+        root <- Node$new("Root", type = "root", decision = "", nd = 1)
+        for (i in 2:nrow(Frame)) {
+            if (i <= 3) parent <- "root"
+            if (i > 3) parent <- paste0("Node", Frame$nd[i] %/% 2)
+            if (Frame$is.terminal[i] == 2) {
+                type <- "terminal"
+                display <- with(
+                    Frame, paste0(nd[i], ") ", tree.split.names(nd[i], nd, p, cutVal, x$vNames, digits), "*"))
+            } else {
+                type <- "interior"
+                display <- with(
+                    Frame, paste0(nd[i], ") ", tree.split.names(nd[i], nd, p, cutVal, x$vNames, digits)))
+            }
+            eval(parse(text = paste0("Node", Frame$nd[i], "<-", parent,
+                                     "$AddChild(display, type = type, nd = Frame$nd[i])")))
+        }
+        SetGraphStyle(root, rankdir = rankdir)
+        if (nodeOnly) {
+            GetNodeLabel <- function(node) {
+                switch(node$type,
+                       root = "Root", 
+                       terminal = paste0("Node ", node$nd),
+                       interior = paste0("Node ", node$nd))
+            }
+            SetNodeStyle(root, fontname = 'helvetica', label = GetNodeLabel, shape = shape)
+        } else {
+            SetNodeStyle(root, fontname = 'helvetica', shape = shape)
+        }
+        ## x$graph <- ToDiagrammeRGraph(x, direction = "climb", pruneFun = NULL)
+        plot.Node(root, output = output, control = list(savePlot = savePlot,
+                                                        file_name = file_name, file_type = file_type))
+    }
 }
 
 ## ---------------------------------------------------------------------------------------
 ## From data.tree package that are not exported by them
 ## ---------------------------------------------------------------------------------------
 
+#' @importFrom data.tree ToDiagrammeRGraph
+#' @keywords internal
+#' @noRd
 plot.Node <- function(x, ..., output = "graph", control) {
     graph <- ToDiagrammeRGraph(x, direction = "climb", pruneFun = NULL)
     if (control$savePlot) {
@@ -112,22 +139,4 @@ plot.Node <- function(x, ..., output = "graph", control) {
     } else {
         render_graph(graph, output = output)
     }
-}
-
-#' Plotting the predicted survival function from an rocTree object
-#'
-#' Plot the predicted survival function from rocTree objects.
-#'
-#' @importFrom ggplot2 ggplot geom_step
-#'
-#' @noRd
-#' @export
-plot.predict.rocTree <- function(x, ..., control = list()) {
-    if (!is.null(control$tau)) x$parm@tau <- control$tau
-    if (!is.null(control$ghN)) x$parm@ghN <- control$ghN 
-    if (!is.predict.rocTree(x)) stop("Response must be a \"predict.rocTree\" object")
-    tmp <- data.frame(x = x$pred$Time, y = x$pred$Surv)
-    gg <- ggplot(tmp, aes(x = x, y = y)) + geom_step(lwd = I(1.1)) +
-        xlab("Time") + ylab("Survival probabilities")
-    gg
 }
